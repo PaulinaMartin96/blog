2419ec09e6a0b490e0719c331452d6ba8bea5880
Paulina Martin <paulinamartin96@ciencias.unam.mx>
2024-06-22 15:43:34 -0600

    update link

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paulinamartin96.github.io',
-  base: "/",
+  base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
